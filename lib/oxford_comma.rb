5c378fe22ae056e1b83ad370211d47eaffe558db
Sarah Sweat <sarahsweat@gmail.com>
2017-05-04 18:14:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    string = array.first
+  elsif array.size == 2
+    string = array.join(" and ")
+  elsif array.size > 2
+    final = array.pop
+    string = array.join(", ") + ", and " + final
+
+  end
+
+
+  string
+
 end
